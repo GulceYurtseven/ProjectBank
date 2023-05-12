b0b3d381ed631dda177ece3f00662ad2bf0feb36
Berşan Gülce Yurtseven <gulceyurtseven@gmail.com>
2023-05-12 20:02:11 +0300

    proje

---
+++ Assets/Scripts/LoginDeneme.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginDeneme: MonoBehaviour
+{
+    public Button login;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void butonCalisma()
+    {
+        Debug.Log("buton");
+        SceneManager.LoadScene(1);
+        Debug.Log("buton");
+        Debug.Log("buton");
+        Debug.Log("buton");
+    }
+}
